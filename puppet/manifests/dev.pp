ee86f86583c5558052e7dcd9b2a07c48a4342877
David Parr <dvdprr6@gmail.com>
2015-05-10 19:45:05 -0400

    set service script global

--- puppet/manifests/dev.pp
+++ puppet/manifests/dev.pp
@@ -5,6 +5,15 @@
 Exec { path => ["/bin/", "/sbin/", "/usr/bin", "/usr/sbin/" ] }
 
 class doc-apt-webapp{
+	file{"/etc/rc.d/init.d/docsaptwebapp":
+		ensure => 'link',
+		target => '/vagrant/server/conf/docsaptwebapp',
+		owner => root,
+		mode => '755',
+	}->
+	file{"/etc/docs_apt/":
+		ensure => 'directory'
+	}
 
 }
 
