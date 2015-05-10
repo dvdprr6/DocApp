9c4dc2e7cca7ee89a630f74c025fa42fa65e9ebc
David Parr <dvdprr6@gmail.com>
2015-05-10 19:46:33 -0400

    setup python environment

---
+++ server/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+install_requires = [
+	'SQLAlchemy==0.9.4',
+	'alembic==0.7.1',
+	'psycopg2==2.5.4',
+	'tornado==4.0.2',
+	'colour-runner'
+]
+
+setup(
+	name='Bank',
+	version=version,
+	description=('Application for doctors to manager their appointments with patients'),
+	author='David Parr',
+	author_email='dvdprr6@gmail.com',
+	packages=find_packages(),
+	install_requires=install_requires,
+	entry_points={
+		'console_scripts':['docsaptwebapp = apt.webserver:main']
+	}
+)
