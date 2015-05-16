a64af50fb1afe9e23dd5db67aaaed4be01eda3ee
David Parr <dvdprr6@gmail.com>
2015-05-16 18:14:52 -0400

    script to initialize the database

---
+++ server/scripts/initdb.py
@@ -0,0 +1,36 @@
+import sys
+import logging
+import argparse
+import configparser
+from sqlalchemy import create_engine
+
+from doctor.db.models import Base
+
+'''
+To get the .db for sqlite
+python scripts/initdb.py --config=conf/test/docs.conf
+'''
+
+def parse_config(config_file):
+	config = configparser.RawConfigParser()
+	config.read(config_file)
+	return config
+
+def initialize_database(config, args):
+	db_url = config.get('sqlalchemy', 'url')
+	db_echo = config.getboolean('sqlalchemy', 'echo')
+	DbEngine = create_engine(db_url, echo=db_echo)
+	Base.metadata.create_all(DbEngine)
+
+def main(argv=sys.argv):
+	parser = argparse.ArgumentParser(description='Doctor DB Init')
+	parser.add_argument('--config', dest='config', required=True)
+	args = parser.parse_args()
+	if not args.config:
+		parser.print_help()
+		sys.exit(1)
+	config = parse_config(args.config)
+	initialize_database(config, args)
+
+if __name__ == '__main__':
+	main()
