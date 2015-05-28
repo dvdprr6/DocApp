01413ab748fa7daee159fdfe23749e5b0cfd7bab
David Parr <dvdprr6@gmail.com>
2015-05-27 21:02:47 -0400

    database model for the doctor appointment app

--- server/doctor/db/models.py
+++ server/doctor/db/models.py
@@ -14,25 +14,17 @@ DbMetadata = Base.metadata
 
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 
-
-'''
-Doctors[] <-> Patients[]
-'''
 doctors_patients_association = Table('doctors_patients', DbMetadata,
-	Column('doctors_id', Integer, ForeignKey('doctors.id'), primary_key=True),
-	Column('patients_id', Integer, ForeignKey('patients.id'), primary_key=True)
+	Column('doctors_id', Integer, ForeignKey('doctors.id')),
+	Column('patients_id', Integer, ForeignKey('patients.id'))
 )
 
-'''
-Specialist[] -> Doctors[]
-'''
 class Specialist(Base):
 	__tablename__ = 'specialist'
 
-	id = Column(Integer, primary_key=True, nullable=False)
+	id = Column(Integer, primary_key=True)
 	specialist_name = Column(String(255))
-
-	doctor_association = relationship('Doctors')
+	doctors_association = relationship('Doctors')
 
 	def to_dict(self):
 		return{
@@ -40,63 +32,47 @@ class Specialist(Base):
 			'specialist_name':self.specialist_name
 		}
 
-'''
-Doctors[] <- Specialist[]
-'''
 class Doctors(Base):
-	__tablename__ = 'doctors'
+	__tablename__ = 'doctors';
 
-	id = Column(Integer, primary_key=True, nullable=False)
-	name = Column(String(255))
-	specialist_id = Column(Integer, ForeignKey('specialist.id'), primary_key=True)
+	id = Column(Integer, primary_key=True)
+	doctors_name = Column(String(255))
+	specialist_id = Column(Integer, ForeignKey('specialist.id'))
 
-	specialist_association = relationship('Specialist', cascade='save-update, merge, delete, delete-orphan')
-
-	patients_association = relationship('Patients', secondary=doctors_patients_association, backref='doctors')
+	doctors_patients_association = relationship('Patients', secondary=doctors_patients_association)
 
 	def to_dict(self):
 		return{
 			'id':self.id,
-			'name':self.name,
+			'doctors_name':self.doctors_name,
 			'specialist_id':self.specialist_id
 		}
 
-
-
-'''
-Patients[] <- PatientStatus[]
-'''
-class Patients(Base):
-	__tablename__ = 'patients'
-
-	id = Column(Integer, primary_key=True, nullable=False)
-	patient_name = Column(String(255))
-	appointment_date = Column(DateTime, default=datetime.datetime.utcnow)
-	status_code_id = Column(Integer, ForeignKey('patient_status.id'), primary_key=True)
-
-	pateint_status_association = relationship('PatientStatus', cascade='save-update, merge, delete, delete-orphan')
-
-	def to_dict(self):
-		return{
-			'id':self.id,
-			'patient_name':self.patient_name,
-			'appointment_date':self.appointment_date.strftime(DATE_FORMAT),
-			'status_code_id':self.status_code_id
-		}
-
-'''
-PatientStatus[] -> Patients[]
-'''
 class PatientStatus(Base):
 	__tablename__ = 'patient_status'
 
-	id = Column(Integer, primary_key=True, nullable=False)
+	id = Column(Integer, primary_key=True)
 	status_code = Column(String(13))
-
 	patient_association = relationship('Patients')
 
 	def to_dict(self):
 		return{
 			'id':self.id,
 			'status_code':self.status_code
+		}
+
+class Patients(Base):
+	__tablename__ = 'patients'
+
+	id = Column(Integer, primary_key=True)
+	patient_name = Column(String(255))
+	appointment_date = Column(DateTime, default=datetime.datetime.utcnow)
+	status_id = Column(Integer, ForeignKey('patient_status.id'))
+
+	def to_dict(self):
+		return{
+			'id':self.id,
+			'patient_name':self.patient_name,
+			'appointment_date':self.appointment_date.strftime(DATE_FORMAT),
+			'status_id':self.status_id
 		}
