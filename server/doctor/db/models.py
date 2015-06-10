4e4410fab87282567c21f991883cc877a62b3aa8
David Parr <dvdprr6@gmail.com>
2015-06-09 20:36:41 -0400

    convert tabs to spaces

--- server/doctor/db/models.py
+++ server/doctor/db/models.py
@@ -3,7 +3,7 @@ import datetime
 import hashlib
 
 from sqlalchemy import (create_engine, Table, Column, Integer,
-	BigInteger, String, DateTime, Text, Boolean, Float, ForeignKey, desc)
+    BigInteger, String, DateTime, Text, Boolean, Float, ForeignKey, desc)
 from sqlalchemy.orm import relationship, column_property, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -15,64 +15,65 @@ DbMetadata = Base.metadata
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 
 doctors_patients_association = Table('doctors_patients', DbMetadata,
-	Column('doctors_id', Integer, ForeignKey('doctors.id')),
-	Column('patients_id', Integer, ForeignKey('patients.id'))
+    Column('doctors_id', Integer, ForeignKey('doctors.id')),
+    Column('patients_id', Integer, ForeignKey('patients.id'))
 )
 
 class Specialist(Base):
-	__tablename__ = 'specialist'
+    __tablename__ = 'specialist'
 
-	id = Column(Integer, primary_key=True)
-	specialist_name = Column(String(255))
-	doctors_association = relationship('Doctors')
+    id = Column(Integer, primary_key=True)
+    specialist_name = Column(String(255))
+    doctors_association = relationship('Doctors')
 
-	def to_dict(self):
-		return{
-			'id':self.id,
-			'specialist_name':self.specialist_name
-		}
+    def to_dict(self):
+        return{
+            'id':self.id,
+            'specialist_name':self.specialist_name
+        }
 
 class Doctors(Base):
-	__tablename__ = 'doctors';
+    __tablename__ = 'doctors';
 
-	id = Column(Integer, primary_key=True)
-	doctors_name = Column(String(255))
-	specialist_id = Column(Integer, ForeignKey('specialist.id'))
+    id = Column(Integer, primary_key=True)
+    doctors_name = Column(String(255))
+    specialist_id = Column(Integer, ForeignKey('specialist.id'))
+    work_hours = Column(Integer, primary_key=False) # per week
 
-	doctors_patients_association = relationship('Patients', secondary=doctors_patients_association)
+    doctors_patients_association = relationship('Patients', secondary=doctors_patients_association)
 
-	def to_dict(self):
-		return{
-			'id':self.id,
-			'doctors_name':self.doctors_name,
-			'specialist_id':self.specialist_id
-		}
+    def to_dict(self):
+        return{
+            'id':self.id,
+            'doctors_name':self.doctors_name,
+            'specialist_id':self.specialist_id
+        }
 
 class PatientStatus(Base):
-	__tablename__ = 'patient_status'
+    __tablename__ = 'patient_status'
 
-	id = Column(Integer, primary_key=True)
-	status_code = Column(String(13))
-	patient_association = relationship('Patients')
+    id = Column(Integer, primary_key=True)
+    status_code = Column(String(13))
+    patient_association = relationship('Patients')
 
-	def to_dict(self):
-		return{
-			'id':self.id,
-			'status_code':self.status_code
-		}
+    def to_dict(self):
+        return{
+            'id':self.id,
+            'status_code':self.status_code
+        }
 
 class Patients(Base):
-	__tablename__ = 'patients'
+    __tablename__ = 'patients'
 
-	id = Column(Integer, primary_key=True)
-	patient_name = Column(String(255))
-	appointment_date = Column(DateTime, default=datetime.datetime.utcnow)
-	status_id = Column(Integer, ForeignKey('patient_status.id'))
+    id = Column(Integer, primary_key=True)
+    patient_name = Column(String(255))
+    appointment_date = Column(DateTime, default=datetime.datetime.utcnow)
+    status_id = Column(Integer, ForeignKey('patient_status.id'))
 
-	def to_dict(self):
-		return{
-			'id':self.id,
-			'patient_name':self.patient_name,
-			'appointment_date':self.appointment_date.strftime(DATE_FORMAT),
-			'status_id':self.status_id
-		}
+    def to_dict(self):
+        return{
+            'id':self.id,
+            'patient_name':self.patient_name,
+            'appointment_date':self.appointment_date.strftime(DATE_FORMAT),
+            'status_id':self.status_id
+        }
--- server/doctor/webserver.py
+++ server/doctor/webserver.py
@@ -1,4 +1,7 @@
 import sys
+import base64
+import uuid
+import json
 import argparse
 import configparser
 import logging
@@ -7,40 +10,64 @@ import logging.config
 import tornado.ioloop
 import tornado.web
 
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from .handlers import doctor_registration_api as doctor_registration_handlers
+
 def parse_config(config_file):
-	config = configparser.RawConfigParser()
-	config.read(config_file)
-	return config
+    config = configparser.RawConfigParser()
+    config.read(config_file)
+    return config
 
-class BaseRequestHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write("Hello, world")
+# class BaseRequestHandler(tornado.web.RequestHandler):
+#   def get(self):
+#       self.write("Hello, world")
 
-application = tornado.web.Application([
-	(r"/", BaseRequestHandler )
-])
+# application = tornado.web.Application([
+#   (r"/", BaseRequestHandler )
+# ])
+
+class WebApplication(tornado.web.Application):
+
+    def __init__(self, config, main_loop=None):
+        self.db = scoped_session(sessionmaker(bind=engine_from_config({
+            'sqlalchemy.url': config.get('sqlalchemy', 'url'),
+            'sqlalchemy.echo': config.getboolean('sqlalchemy', 'echo')
+        })))
+        logging.info('webapplication')
+        settings = {
+            'debug': True,
+            'cookie_secret': str(base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes))
+        }
+
+        handlers = []
+        handlers += doctor_registration_handlers
+        tornado.web.Application.__init__(self, handlers, **settings)
+
 
 def main():
-	parser = argparse.ArgumentParser(description="Doc webapp init");
-	parser.add_argument('--config', dest='config', required=True);
-	args = parser.parse_args()
-	if not args.config:
-		parser.print_help()
-		sys.exit(1)
+    parser = argparse.ArgumentParser(description="Doc webapp init");
+    parser.add_argument('--config', dest='config', required=True);
+    args = parser.parse_args()
+    if not args.config:
+        parser.print_help()
+        sys.exit(1)
 
-	config = parse_config(args.config)
-	if not config.has_option('docwebapp', 'port'):
-		print('port is required')
-		sys.exit(1);
+    config = parse_config(args.config)
+    if not config.has_option('docwebapp', 'port'):
+        print('port is required')
+        sys.exit(1);
 
-	logging.config.fileConfig(args.config, disable_existing_loggers=0)
-	logging.getLogger('tornado').setLevel(config.getint('docwebapp', 'logging'))
-	logging.getLogger('webserver').info('<!> doc\'s webapp being initialized...')
-	'''INIT SERVER'''
-	main_loop = tornado.ioloop.IOLoop.instance()
-	application.listen(config.getint('docwebapp', 'port'))
-	logging.getLogger('webserver').info('<!> doc\'s webapp initialized (version = %s)' % ('0.0.1'))
-	main_loop.start()
+    logging.config.fileConfig(args.config, disable_existing_loggers=0)
+    logging.getLogger('tornado').setLevel(config.getint('docwebapp', 'logging'))
+    logging.getLogger('webserver').info('<!> doc\'s webapp being initialized...')
+    '''INIT SERVER'''
+    main_loop = tornado.ioloop.IOLoop.instance()
+    application = WebApplication(config=config, main_loop=main_loop)
+    application.listen(config.getint('docwebapp', 'port'))
+    logging.getLogger('webserver').info('<!> doc\'s webapp initialized (version = %s)' % ('0.0.1'))
+    main_loop.start()
 
 if __name__ == "__main__":
-	main()
+    main()
