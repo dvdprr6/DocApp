89e6fb0cdf45c475a59eb419b6848d73756cf8c6
David Parr <dvdprr6@gmail.com>
2015-06-09 20:43:06 -0400

    main handler for webserver

---
+++ server/doctor/handlers.py
@@ -0,0 +1,26 @@
+import logging
+import json
+
+import tornado.web
+
+logging = logging.getLogger(__name__)
+
+class BaseRequestHandler(tornado.web.RequestHandler):
+
+    @property
+    def db(self):
+        return self.application.db
+
+    @property
+    def request_body_json(self):
+        return json.loads(self.request.body.decode('utf-8'))
+
+'''
+Doctor Registration API
+'''
+
+from .doctor_registration.handlers import MainHandler
+
+doctor_registration_api = [
+    (r'/doctor_registration/?', MainHandler)
+]
