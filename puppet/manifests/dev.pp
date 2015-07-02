6db303a3fee5108f57d8040366c40ea5d4a86464
David Parr <dvdprr6@gmail.com>
2015-07-01 21:03:59 -0400

    set localtime

--- puppet/manifests/dev.pp
+++ puppet/manifests/dev.pp
@@ -5,15 +5,25 @@
 Exec { path => ["/bin/", "/sbin/", "/usr/bin", "/usr/sbin/" ] }
 
 class doc-apt-webapp{
-	file{"/etc/rc.d/init.d/docsaptwebapp":
-		ensure => 'link',
-		target => '/vagrant/server/conf/docsaptwebapp',
-		owner => root,
-		mode => '755',
-	}->
-	file{"/etc/docs_apt/":
-		ensure => 'directory'
-	}
+    file{"/etc/rc.d/init.d/docsaptwebapp":
+        ensure => 'link',
+        target => '/vagrant/server/conf/docsaptwebapp',
+        owner => root,
+        mode => '755',
+    }->
+    file{"/etc/docs_apt/":
+        ensure => 'directory'
+    }->
+    exec{"unlink-localtime":
+        command => 'unlink /etc/localtime',
+        cwd => '/home/vagrant',
+        user => root
+    }->
+    file{"/etc/localtime":
+        ensure => 'link',
+        target => '/usr/share/zoneinfo/Canada/Eastern',
+        owner => root
+    }
 
 }
 
