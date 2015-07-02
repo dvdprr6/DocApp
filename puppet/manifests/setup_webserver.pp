bfa5a3a0a029b08c006df7be5d9fe78830374a08
David Parr <dvdprr6@gmail.com>
2015-07-01 21:03:09 -0400

    changed tabs to spaces

--- puppet/manifests/setup_webserver.pp
+++ puppet/manifests/setup_webserver.pp
@@ -3,50 +3,50 @@
 #
 
 class setup_webserver{
-	$work_path = '/usr/local/bin/:/usr/local/:/usr/bin/:/bin/:/opt/python3.4/bin'
-	exec{"python-env":
-		command => '/opt/python3.4/bin/python3.4 -m venv /home/vagrant/pyEnv',
-		cwd => '/home/vagrant',
-		path => $work_path,
-		user => vagrant
-	}->
-	exec{"install-setuptools":
-		command => 'curl https://bootstrap.pypa.io/ez_setup.py -o - | /home/vagrant/pyEnv/bin/python',
-		cwd => '/home/vagrant',
-		path => $work_path,
-		user => vagrant,
-		timeout => 0
-	}->
-	exec{"install-pip":
-		command => '/home/vagrant/pyEnv/bin/easy_install pip',
-		cwd => '/home/vagrant',
-		path => $work_path,
-		user => vagrant,
-		timeout => 0
-	}->
-	exec{"setup":
-		command => '/home/vagrant/pyEnv/bin/python setup.py develop',
-		cwd => '/vagrant/server',
-		path => $work_path,
-		user => vagrant,
-		timeout => 0
-	}->
-	file{"/etc/docs_apt/docs.conf":
-		ensure => "file",
-		source => "file:///vagrant/server/conf/dev/docs.conf"
-	}->
-	exec{"init-database":
-		command => '/home/vagrant/pyEnv/bin/python scripts/initdb.py --config=/etc/docs_apt/docs.conf',
-		cwd => '/vagrant/server',
-		path => $work_path,
-		user => vagrant
-	}->
-	exec{"python-at-startup":
-		command => 'echo ". /home/vagrant/pyEnv/bin/activate" >> /home/vagrant/.bashrc',
-		cwd => '/home/vagrant',
-		path => $work_path,
-		user => vagrant
-	}
+    $work_path = '/usr/local/bin/:/usr/local/:/usr/bin/:/bin/:/opt/python3.4/bin'
+    exec{"python-env":
+        command => '/opt/python3.4/bin/python3.4 -m venv /home/vagrant/pyEnv',
+        cwd => '/home/vagrant',
+        path => $work_path,
+        user => vagrant
+    }->
+    exec{"install-setuptools":
+        command => 'curl https://bootstrap.pypa.io/ez_setup.py -o - | /home/vagrant/pyEnv/bin/python',
+        cwd => '/home/vagrant',
+        path => $work_path,
+        user => vagrant,
+        timeout => 0
+    }->
+    exec{"install-pip":
+        command => '/home/vagrant/pyEnv/bin/easy_install pip',
+        cwd => '/home/vagrant',
+        path => $work_path,
+        user => vagrant,
+        timeout => 0
+    }->
+    exec{"setup":
+        command => '/home/vagrant/pyEnv/bin/python setup.py develop',
+        cwd => '/vagrant/server',
+        path => $work_path,
+        user => vagrant,
+        timeout => 0
+    }->
+    file{"/etc/docs_apt/docs.conf":
+        ensure => "file",
+        source => "file:///vagrant/server/conf/dev/docs.conf"
+    }->
+    exec{"init-database":
+        command => '/home/vagrant/pyEnv/bin/python scripts/initdb.py --config=/etc/docs_apt/docs.conf',
+        cwd => '/vagrant/server',
+        path => $work_path,
+        user => vagrant
+    }->
+    exec{"python-at-startup":
+        command => 'echo ". /home/vagrant/pyEnv/bin/activate" >> /home/vagrant/.bashrc',
+        cwd => '/home/vagrant',
+        path => $work_path,
+        user => vagrant
+    }
 }
 
 include setup_webserver
