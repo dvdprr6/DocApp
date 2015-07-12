91817d4c654bc25506f55b87d4f0d957ed44760e
David Parr <dvdprr6@gmail.com>
2015-07-12 15:47:58 -0400

    test data for adding doctor test case

--- server/tests/doctor_registration/test_data.py
+++ server/tests/doctor_registration/test_data.py
@@ -1,3 +1,34 @@
+from datetime import date
+
+current_date = date.today()
+DATE_FORMAT = '%Y-%m-%d'
+
 ENDPOINTS = {
     'home':'/doctor_registration/'
 }
+
+TEST_REGISTER_DOCTOR = {
+    'doctors_name':'Frankenstein',
+    'specialist_name':'death',
+    'work_hours': 9999
+}
+
+TEST_EXPECTED_RETURN_DOCTOR = {
+    'id':1,
+    'doctors_name':'Frankenstein',
+    'work_hours':9999,
+    'registration_date':current_date.strftime(DATE_FORMAT)
+}
+
+TEST_EXPECTED_RETURN_SPECIALIST = {
+    'id':1,
+    'specialist_name':'death'
+}
+
+
+REGISTER_DOCTOR_RETURN_ATTRIBUTES = [
+    'doctors_name',
+    'specialist_name',
+    'work_hours',
+    'registration_date'
+]
