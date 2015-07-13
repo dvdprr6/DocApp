97189711beca4d2479f8e009059911c1b1d61e23
David Parr <dvdprr6@gmail.com>
2015-07-12 21:55:37 -0400

    initialize tables for test case

--- server/tests/doctor_registration/common.py
+++ server/tests/doctor_registration/common.py
@@ -5,7 +5,13 @@ import logging
 from tornado.web import create_signed_value
 
 from tests.core import BaseAppTestCase
-from doctor.db.models import (Doctors, Specialist)
+
+from doctor.db.models import (
+    Doctors,
+    Specialist
+)
+
+from .test_data import INITIALIZE_DOCTOR_REGISTATION
 
 logging = logging.getLogger(__name__)
 
@@ -37,3 +43,58 @@ class DoctorRegistrationAPITestCase(BaseAppTestCase):
             'Cookie':'='.join(secure_cookie)
         }
         return headers
+
+
+class DoctorRegistrationTestCase(DoctorRegistrationAPITestCase):
+
+    def setUp(self):
+        super().setUp()
+        self._add_doctors_and_specialist_to_tables()
+
+    def tearDown(self):
+        self._remove_doctors_and_specialists_from_tables()
+        super().tearDown()
+
+    def _add_doctors_and_specialist_to_tables(self):
+        for doctor_info in INITIALIZE_DOCTOR_REGISTATION:
+            doctors_name = doctor_info['doctors_name']
+            specialist_name = doctor_info['specialist_name']
+            work_hours = doctor_info['work_hours']
+
+            add_specialist = Specialist(specialist_name=specialist_name)
+            self.db.add(add_specialist)
+            self.db.commit()
+            specialist_response_body = {
+                'data':{
+                    'specialist_data':add_specialist.to_dict()
+                }
+            }
+            # print(specialist_response_body)
+
+            add_doctor = Doctors(
+                doctors_name=doctors_name,
+                specialist_id=specialist_response_body['data']['specialist_data']['id'],
+                work_hours=work_hours
+            )
+            self.db.add(add_doctor)
+            self.db.commit()
+            doctor_response_body = {
+                'data':{
+                    'doctors_data':add_doctor.to_dict()
+                }
+            }
+            # print(doctor_response_body)
+
+        self._doctor_data_info = self.db.query(Doctors).all()
+        self._specialist_data_info = self.db.query(Specialist).all()
+
+    def _remove_doctors_and_specialists_from_tables(self):
+        for doctor in self._doctor_data_info:
+            self.db.delete(doctor)
+        self.db.commit()
+        self._doctor_data_info = []
+
+        for specialist in self._specialist_data_info:
+            self.db.delete(specialist)
+        self.db.commit()
+        self._specialist_data_info = []
